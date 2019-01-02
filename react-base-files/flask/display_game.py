c898defd6c46f8be0da00344b5a3522ec3a0d151
Josh P <jphillip44@gmail.com>
2019-01-02 14:11:22 -0500

    basic visitor like demo for display_game

--- react-base-files/flask/display_game.py
+++ react-base-files/flask/display_game.py
@@ -4,7 +4,10 @@ import games
 class DisplayGame():
 
     def switch(self, obj):
-        getattr(self, obj.__name__)(obj)
+        getattr(self, obj.__class__.__name__)(obj)
+
+    def list(self, obj):
+        print(*obj, sep='\n')
 
     def Double07(self, obj):
         obj.display()
@@ -14,6 +17,8 @@ class DisplayGame():
 
 
 if __name__ == '__main__':
+    player_list = ['player1', 'player2']
+    DisplayGame().switch(player_list)
     game = games.Double07(['A', 'B'])
     DisplayGame().switch(game)
     game = games.Hot_Potato(['A', 'B'])
