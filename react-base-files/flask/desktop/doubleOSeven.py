7f9886909a0677fdd605b44dae22dfe5dc2b4bca
ACoort <45796585+ACoort@users.noreply.github.com>
2019-01-14 21:44:11 -0500

    Fixed Josh trying to make me do shit again xd

--- react-base-files/flask/desktop/doubleOSeven.py
+++ react-base-files/flask/desktop/doubleOSeven.py
@@ -2,6 +2,7 @@ from tkinter import Tk, Label, Frame
 from tkinter.font import Font
 import math
 import os
+import time
 from pathlib import Path
 from PIL import Image, ImageTk
 from __screen import DesktopUI
@@ -21,7 +22,7 @@ class Double07UI():
         self.window = ui
         self.setup()
         self.display(obj['players'], obj['timer'])
-        self.timer(obj['timer'])
+        self.timer(obj['timer'], obj['players'])
 
     def setup(self):
         topFrame = Frame(height = self.window.screenH / 10, width = self.window.screenW, bg= self.window.backgroundC)
@@ -136,9 +137,13 @@ class Double07UI():
         ph = ImageTk.PhotoImage(img)
         return ph
 
-    def timer (self, timer):
-        topFrameLabel = Label(self.topframe, text = "Time Remaining to select an action: " + str(timer), font = self.window.deffont, bg = self.rightframe['bg'], fg = "white") #need to add to top frame
-        topFrameLabel.place(anchor = "center", y = self.window.screenH / 20, x = self.window.screenW / 2) 
+    def timer (self, timer, players):
+        if timer >= 0:
+            topFrameLabel = Label(self.topframe, text = "Time Remaining to select an action: " + str(timer), font = self.window.deffont, bg = self.rightframe['bg'], fg = "white") #need to add to top frame
+            topFrameLabel.place(anchor = "center", y = self.window.screenH / 20, x = self.window.screenW / 2) 
+        
+        if timer == -1:
+            self.eventlog(players)
 
     def heartDisplay(self, number):
         path = os.path.join(os.path.relpath(os.path.dirname(__file__)),  '../../public/images')
@@ -162,6 +167,111 @@ class Double07UI():
         ph = ImageTk.PhotoImage(img)
 
         return ph
+
+    def eventlog(self, players):
+        playerActed = []
+        
+        act = 0
+        result = 0
+        posCounter = 0
+
+        ELFont = self.window.setFontSize(int (self.window.screenH / 30))
+        textColour = "white"
+
+        path = os.path.join(os.path.relpath(os.path.dirname(__file__)),  '../../public/images')
+        imageFolder = Path(path)
+        
+        for player in players:
+            if players[player].get('hp') > 0 and players[player].get('hp') != "dead":
+
+                curPlayerAction = players[player].get('defend')
+                
+                if curPlayerAction != "none" or curPlayerAction != "all":
+                
+                    oppAct = players[curPlayerAction].get('defend')
+                    playerActed.append(player)
+
+                    if oppAct == 'defend':
+                        act = "Attack.png"
+                        result = "Defend.png"
+                        playerActed.append(curPlayerAction)
+                    elif oppAct == 'none' or oppAct != player:
+                        act = "Attack.png"
+                        result = "Heart.png"
+                    else:
+                        act = "CAttack.png"
+                        result = "none"
+                        playerActed.append(curPlayerAction)
+
+                    posY = self.window.screenH * (1/10 + 1/10 * posCounter)
+
+                    label1 = Label(self.centerframe, text = player, font = ELFont, bg = self.centerframe['bg'], fg = textColour)
+                    label1.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 1/40)
+
+                    imgFile = os.path.join(imageFolder, act)
+                    img = Image.open(imgFile)
+                    img = img.resize((int(self.window.screenW / 15), int(self.window.screenH / 20)))
+
+                    actImg = ImageTk.PhotoImage(img)
+                    
+                    label2 = Label(self.centerframe, image = actImg, bg = self.centerframe['bg']) 
+                    label2.image = actImg 
+                    label2.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 17/40)
+
+                    label3 = Label(self.centerframe, text = curPlayerAction, font = ELFont, bg = self.centerframe['bg'], fg = textColour)
+                    label3.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 20.5/40)
+
+                    if result != 'none':
+                        print("not reloading")
+                        imgFile2 = os.path.join(imageFolder, result)
+                        img2 = Image.open(imgFile2)
+                        img2 = img2.resize((int(self.window.screenW / 15), int(self.window.screenH / 20)))
+
+                        resImg = ImageTk.PhotoImage(img2)
+
+                        label4 = Label(self.centerframe, image = resImg, bg = self.centerframe['bg']) 
+                        label4.image = resImg 
+                        label4.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 36.5/40)
+
+                    posCounter += 1
+                    time.sleep(1)
+                    
+            else:
+                playerActed.append(player)
+
+
+       
+        for player in players:
+            act = 0
+
+            if not (player in playerActed):
+                playerAct = players[player].get('defend')
+                if playerAct == "none":
+                    act = "Reload.png"
+                else:
+                    act = "Defend.png"
+                    print(playerAct)
+                
+                posY = 1/10 + 1/10 * posCounter
+
+                label1 = Label(self.centerframe, text = player, font = ELFont, bg = self.centerframe['bg'], fg = textColour)
+                label1.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 1/40)
+
+                imgFile = os.path.join(imageFolder, act)
+                img = Image.open(imgFile)
+                img = img.resize((int(self.window.screenW / 15), int(self.window.screenH / 20)))
+
+                actImg = ImageTk.PhotoImage(img)
+                
+                label2 = Label(self.centerframe, image = actImg, bg = self.centerframe['bg']) 
+                label2.image = actImg 
+                label2.place(anchor = "nw", y = posY, x = self.window.screenW / 4 + 17/40)
+                time.sleep(1)
+
+        
+        self.window.win.update()   
+        print("show?")
+        time.sleep(5)  
 
 def main():
     pass
