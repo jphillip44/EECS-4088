71fff4d641161d536259fe02413259f23c080214
Edwin Santos <31744511+EdwinSantos@users.noreply.github.com>
2019-02-28 10:48:53 -0500

    Update instructions.py
    Made some slight adjustments to instructions.

--- react-base-files/flask/instructions.py
+++ react-base-files/flask/instructions.py
@@ -12,7 +12,7 @@ class Instructions():
 Each player starts with 3 life points and 2 action points
 Defending and attacking cost 1 action point
 Reloading grants 1 action point
-Attacking a player that is reloading or attacking another players results in them losing 1 life point and the attacker gaining 1 action point
+Attacking a player that is reloading or attacking a player that is attacking another player results in them losing 1 life point and the attacker gaining 1 action point
 Defending against an attack from another player will grant the player defending 1 action point
 Once a player loses all of their life points they are eliminated
 The goal is to be the last player with life points
@@ -31,7 +31,7 @@ If the potato explodes in your possession the seconds you held the potato will b
         self.string = """
 Users will be split into teams of 2.
 One player at a time will be tasked with picking a card from the board of cards displayed on the main computer screen by navigating with the directional pad on their mobile device.
-A selected card will be turned face up.
+A selected card will be turned face up on their mobile device.
 Once the user picks a card their partner will attempt to find that cards matching pair by navigating through the board and selecting another card.
 If the team member does not find the matching card both cards are turned face down and that teams turn ends.
 If the team member finds the matching card then those 2 cards are removed from the board and that team is awarded a point. 
@@ -43,6 +43,7 @@ The team with the most points once the board has been cleared is the winner.
 An image will be shown on the main computer screen. 
 Your task is to select the image on your mobile device that is part of the larger image on the computer screen. 
 Users who select the correct fragment will be awarded a point.
+The faster you select the correct image the more points you will be awarded.
 The user with the most points at the end of the round wins the game.
         """
 
@@ -56,6 +57,6 @@ Your task is to tap the button on your screen as many times as is indicated on t
 Simon
 A series of squares of varying colors are shown on the computer screen. Your task is to enter the combination of colors on your mobile device.
 
-Quickmaff
+Quick maffs
 A simple math equation will be shown on the computer screen. You will be prompted to enter the result on your handheld device.
         """
