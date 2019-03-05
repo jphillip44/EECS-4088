2e35635fa129c0f6ebba8137009e5256e3069d7f
jphillip44 <jphillip44@gmail.com>
2019-03-05 14:55:13 -0500

    fixed timer in multigame

--- flask/app.py
+++ flask/app.py
@@ -109,7 +109,7 @@ def disconnect():
         return
     display()
     global INSTRUCTIONS
-    if not INSTRUCTIONS and GAME is None and not GAME.active:
+    if not INSTRUCTIONS and GAME is None:
         DISPLAY.update(Players())
     else:
         GAME.remove_player(user)
